29f42e0e5942e6d583b6dd19dbbd11091f8aea71
LebLeb03 <144964929+LebLeb03@users.noreply.github.com>
2025-03-28 00:38:52 +0000

    added Index to test

--- wp-content/themes/chatbot/functions.php
+++ wp-content/themes/chatbot/functions.php
@@ -11,7 +11,7 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
     wp_enqueue_script('custom_javascript', get_stylesheet_directory_uri() . '/customElements.js');
-    wp_enqueue_script_module('custom_module1',"https://lebleb03.github.io/Assignment_1_UX308/Order.js");
+    wp_enqueue_script_module('custom_module1',"https://lebleb03.github.io/Assignment_1_UX308/Index.js");
     wp_enqueue_script_module('custom_module2',"https://rhildred.github.io/chatter/fab.js");
 }
 
