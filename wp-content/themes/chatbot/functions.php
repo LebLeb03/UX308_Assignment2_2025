436e20462520445ba69bc7574c5122a819e1f983
LebLeb03 <144964929+LebLeb03@users.noreply.github.com>
2025-03-29 01:34:50 +0000

    added fab back

--- wp-content/themes/chatbot/functions.php
+++ wp-content/themes/chatbot/functions.php
@@ -12,7 +12,7 @@ function theme_enqueue_styles() {
     );
     wp_enqueue_script('custom_javascript', get_stylesheet_directory_uri() . '/customElements.js');
     wp_enqueue_script_module('custom_module1',"https://lebleb03.github.io/Assignment_1_UX308/index.js");
-    wp_enqueue_script_module('custom_module2',"https://lebleb03.github.io/Test_A_Fix/fab.js");
+    wp_enqueue_script_module('custom_module2',"https://rhildred.github.io/chatter/fab.js");
 }
 
 //
